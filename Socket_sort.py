9abca97a421d81f2937cbb513dc95deb57abb807
Julie Léon <julie.leon@etu.minesparis.psl.eu>
2024-01-23 15:18:17 +0100

    Premier message

--- Socket_sort.py
+++ Socket_sort.py
@@ -3,15 +3,35 @@ import argparse
 
 #Constantes
 PORT = 32000
+MESSAGE1 = b'Hello'
 
-# create an INET, STREAMing socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# now connect to the web server on port - the normal http port
-s.connect(("localhost", PORT))
-# bind the socket to a public host, and a well-known port
-s.bind((socket.gethostname(), PORT))
-# become a server socket
-s.listen()
+#Arguments
+parser = argparse.ArgumentParser(description='A parallelized sort usinig sockets', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-s','--server',action='store_true', help='Run as server')
+args = parser.parse_args()
+
+if args.server :
+    # create an INET, STREAMing socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # bind the socket to a public host, and a well-known port
+    s.bind((socket.gethostname(), PORT)) 
+    # become a server socket
+    s.listen()
+    (clientsocket, address) = s.accept()
+    text = clientsocket.recv(len(MESSAGE1))
+
+else :
+    # create an INET, STREAMing socket
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # now connect to the web server on port - the normal http port
+    serversocket.connect(("localhost", PORT))
+    serversocket.send(MESSAGE1)
+
+
+
+
+
+
 
 
 """while True:
