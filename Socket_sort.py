6b39ceb1aa1caa455dddf8747de9fe78d6bd81d4
Julie Léon <julie.leon@etu.minesparis.psl.eu>
2024-01-23 16:01:53 +0100

    Message 2

--- Socket_sort.py
+++ Socket_sort.py
@@ -4,28 +4,33 @@ import argparse
 #Constantes
 PORT = 32000
 MESSAGE1 = b'Hello'
+MESSAGE2 = b'Hi'
 
 #Arguments
 parser = argparse.ArgumentParser(description='A parallelized sort usinig sockets', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-s','--server',action='store_true', help='Run as server')
+parser.add_argument('--host', default='localhost')
 args = parser.parse_args()
 
+# create an INET, STREAMing socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 if args.server :
-    # create an INET, STREAMing socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # bind the socket to a public host, and a well-known port
-    s.bind((socket.gethostname(), PORT)) 
+    s.bind(('', PORT)) 
     # become a server socket
     s.listen()
     (clientsocket, address) = s.accept()
     text = clientsocket.recv(len(MESSAGE1))
+    print(str(text))
+    clientsocket.send(MESSAGE2)
 
 else :
-    # create an INET, STREAMing socket
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # now connect to the web server on port - the normal http port
-    serversocket.connect(("localhost", PORT))
-    serversocket.send(MESSAGE1)
+    # now connect to the web server on port - the normal http port
+    s.connect((args.host, PORT))
+    s.send(MESSAGE1)
+    text2 = s.recv(len(MESSAGE2))
+    print(str(text2))
 
 
 
